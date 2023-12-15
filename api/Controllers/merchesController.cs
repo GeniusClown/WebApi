329941200908f6ee1f662f5ff6dc685937f9acbb
kaneki it <laborotorydeath@yandex.ru>
2023-12-15 15:13:01 +0300

    gg

---
+++ api/Controllers/merchesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using api.Entities;
+using api.Models;
+
+namespace api.Controllers
+{
+    public class merchesController : ApiController
+    {
+        private DVDmagazinEntities1 db = new DVDmagazinEntities1();
+
+        // GET: api/merches
+        [ResponseType(typeof(List<ResponseMerch>))]
+        public IHttpActionResult Getmerches()
+        {
+            return Ok(db.merch.ToList().ConvertAll(p => new ResponseMerch(p)));
+        }
+
+        // GET: api/merches/5
+        [ResponseType(typeof(merch))]
+        public IHttpActionResult Getmerch(int id)
+        {
+            merch merch = db.merch.Find(id);
+            if (merch == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(merch);
+        }
+
+        // PUT: api/merches/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putmerch(int id, merch merch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != merch.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(merch).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!merchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/merches
+        [ResponseType(typeof(merch))]
+        public IHttpActionResult Postmerch(merch merch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.merch.Add(merch);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = merch.id }, merch);
+        }
+
+        // DELETE: api/merches/5
+        [ResponseType(typeof(merch))]
+        public IHttpActionResult Deletemerch(int id)
+        {
+            merch merch = db.merch.Find(id);
+            if (merch == null)
+            {
+                return NotFound();
+            }
+
+            db.merch.Remove(merch);
+            db.SaveChanges();
+
+            return Ok(merch);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool merchExists(int id)
+        {
+            return db.merch.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ api/Models/ResponseMerch.cs
@@ -0,0 +1,28 @@
+﻿using api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api.Models
+{
+    public class ResponseMerch
+    {
+        public ResponseMerch(merch merch)
+        {
+            id = merch.id;
+            name = merch.name;
+            dexcription = merch.dexcription;
+            manufacturer = merch.manufacturer;
+            price = merch.price;
+            photo = merch.photo;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string dexcription { get; set; }
+        public string manufacturer { get; set; }
+        public int price { get; set; }
+        public byte[] photo { get; set; }
+    }
+}
